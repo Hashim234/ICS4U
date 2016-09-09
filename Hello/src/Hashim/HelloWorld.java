4ee56c2b0c390fada1d3c8838cb50f228f46d0ea
324350081 <324350081@C-DNY5WR1.tdsb.on.ca>
2016-09-09 13:38:30 -0400

    Adding HelloWorld, Greetings and Greetings2

---
+++ Hello/src/Hashim/HelloWorld.java
@@ -0,0 +1,12 @@
+package Hashim;
+
+/**
+ * Displays a "Hello World!" message on the screen
+ */
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
