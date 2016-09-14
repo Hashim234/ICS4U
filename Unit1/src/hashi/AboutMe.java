f038c88622c18b6e076ccf26b4c24b9b70003247
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-14 13:35:33 -0400

    about me program

---
+++ Unit1/src/hashi/AboutMe.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package hashi;
+
+/** ISC3U
+ *  AboutMe
+ * 14/09/16
+ * Hashim Hashi
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	  System.out.println("HashimH");
+	  System.out.println("Ms.Kemp");
+	  System.out.println("Runnymede C.I");
+	  System.out.println("GoRavens\"");
+	  
+	  System.out.format("%-10s %8s", "Weekdays"
+	  
+
+	}
+
+}
