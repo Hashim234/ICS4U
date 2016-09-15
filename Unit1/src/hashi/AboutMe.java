039de2d2d1440ec60118a852bd6f0ac37170db28
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-15 13:42:32 -0400

    cpu

--- Unit1/src/hashi/AboutMe.java
+++ Unit1/src/hashi/AboutMe.java
@@ -10,17 +10,18 @@ package hashi;
  */
 public class AboutMe {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 	  System.out.println("HashimH");
 	  System.out.println("Ms.Kemp");
 	  System.out.println("Runnymede C.I");
 	  System.out.println("GoRavens\"");
-	  
-	  System.out.format("%-10s %8s", "Weekdays"
-	  
+	  System.out.format("\n");
+	  System.out.format("%-10s %8s %18s", "Weekdays,\t", "ClassCode,\t", "Class Start Times \n");
+      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "BAF3M1-01\t", "8:45am\n");
+      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "SCH3U1-01\t", "10:07am\n"); 
+      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "ICS3U1-01\t", "12:26am\n");
+      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "PP3OM1-01\t", "01:48am\n");
+    
 
 	}
 
---
+++ Unit1/src/hashi/Bingo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hashi;
+
+/**ICS3U
+ * Bingo
+ * 15/09/16
+ * Hashim Hashi
+ */
+public class Bingo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
