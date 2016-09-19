8b53474572af9ab013f53539900083bedde739df
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-19 12:44:57 -0400

    about me

--- Unit1/src/hashi/AboutMe.java
+++ Unit1/src/hashi/AboutMe.java
@@ -19,8 +19,8 @@ public class AboutMe {
 	  System.out.format("%-10s %8s %18s", "Weekdays,\t", "ClassCode,\t", "Class Start Times \n");
       System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "BAF3M1-01\t", "8:45am\n");
       System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "SCH3U1-01\t", "10:07am\n"); 
-      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "ICS3U1-01\t", "12:26am\n");
-      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "PP3OM1-01\t", "01:48am\n");
+      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "ICS3U1-01\t", "12:26pm\n");
+      System.out.format("%-10s %8s %18s", "Monday-Friday, \t", "PP3OM1-01\t", "01:48pm\n");
     
 
 	}
