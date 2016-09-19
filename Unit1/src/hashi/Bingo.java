21298a3425b30fc35b94565ef624b47f4c166d8e
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-19 13:08:16 -0400

    The Bingo Card

--- Unit1/src/hashi/Bingo.java
+++ Unit1/src/hashi/Bingo.java
@@ -14,8 +14,20 @@ public class Bingo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 System.out.format("%5s%5s%5s%5s%5s", "B", "I", "N", "G", "O\n");
+	 System.out.format("%5s%5s%5s%5s%5s", "5", "3", "9", "12", "15\n");
+	 System.out.format("%5s%5s%5s%5s%5s", "17", "21", "24", "18", "30\n" );
+	 System.out.format("%5s%5s%6s%4s%5s", "33", "37", "FREE", "45", "32\n" );
+	 System.out.format("%5s%5s%5s%5s%5s", "60", "47", "49", "54", "58\n" );
+	 System.out.format("%5s%5s%5s%5s%5s", "62", "70", "65", "69", "74\n" );
+	
+	
+	
+	
 	}
+	
+	
+	
+	
 
 }
