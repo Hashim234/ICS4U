27f74e4cadb418c7cca1f6a013146ce6a1120c0c
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-19 13:38:23 -0400

    the game

---
+++ Unit1/src/hashi/TicTacToeBoard.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package hashi;
+
+/**
+ * @author 324350081
+ *
+ */
+public class TicTacToeBoard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("\t|\t|"); 
+		System.out.println("\t|\t|"); 
+		System.out.println("\t|\t|"); 
+		System.out.println("  --------------------");
+		System.out.println("\t|\t|"); 
+		System.out.println("\t|   X \t|"); 
+		System.out.println("\t|\t|"); 
+		System.out.println("  --------------------");
+		System.out.println("\t|\t|"); 
+		System.out.println("\t|\t|"); 
+		System.out.println("\t|\t|"); 
+	}
+
+}
