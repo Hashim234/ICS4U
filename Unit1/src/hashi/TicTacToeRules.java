4b39b1079bc32b298599746dd5d80efa48ba7fc1
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-20 12:42:38 -0400

    rules

---
+++ Unit1/src/hashi/TicTacToeRules.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package hashi;
+
+/**ICS3U
+ * TicTacToe Rules 
+ * Hashim Hashi
+ *20/09/16
+ */
+public class TicTacToeRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		 System.out.format("%5s%5s", "Tic Tac Toe Rules", "\n");
+		 System.out.format("\n");
+		 System.out.format(" Tic Tac Toe is a game for 2 players\n");
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "1.","A grid of 9 squares is drawn.\n");
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "2.", "The first player draws an X in an empty square.\n" );
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "3.", "The second player draws an O in an empty square.\n" );
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "4.","Steps 2 and 3 are repeated until a tic tac Pattern is created: Patterns can be diagnol, horizontal, or verical.\n" );
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "5.", "Aw innig pattern is completed by drawing a line through the pattern.\n" );
+	}
+
+}
