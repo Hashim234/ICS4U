b7458e6890a40fc2ca39df8b862ccc3f534460be
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-20 12:29:15 -0400

    new one

--- Unit1/src/hashi/BingoRules.java
+++ Unit1/src/hashi/BingoRules.java
@@ -16,11 +16,17 @@ public class BingoRules {
 	public static void main(String[] args) {
 	
 		 System.out.format("%5s%5s", "Bingo Card", "\n");
-		 System.out.format("%5s%5s", "1.",  "The caller randomly pulls a number bingo ball.\n");
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "1.","The caller randomly pulls a number bingo ball.\n");
+		 System.out.format("\n");
 		 System.out.format("%5s%5s", "2.", "The number is placed on the bingo board and called out.\n" );
+		 System.out.format("\n");
 		 System.out.format("%5s%5s", "3.", "Players look for the called number on their bingo card.\n" );
+		 System.out.format("\n");
 		 System.out.format("%5s%5s", "4.","If the number is located, it is marked off.\n" );
+		 System.out.format("\n");
 		 System.out.format("%5s%5s", "5.", "Steps 1 to 4 are repeated until a player matches the bingo pattern.\n" );
+		 System.out.format("\n");
          System.out.format("%5s%5s", "6.", "The winning player yells bingo.\n");
 	}
 
