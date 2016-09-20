4bec83d0f542d10cc6ef878b511131b43e643d1d
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-20 13:14:30 -0400

    binarynumbers

---
+++ Unit1/src/hashi/BinaryNumbers.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package hashi;
+
+/**ICS3U
+ * BinaryNumbers 
+ * Hashim Hashi
+ *20/09/16
+ */
+public class BinaryNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%4s%17s%17s%17s","Decimal ","Binary","Decimal","Hexadecimal\n"); 
+		 System.out.format("%4s%16s%16s%16s","      1  ","   1","           1  ","    0\n");
+		 System.out.format("%4s%16s%16s%16s","      2  ","   10","      2  ","    1\n");
+		 System.out.format("%4s%16s%16s%16s","      3  ","   11","      3  ","    2\n");
+		 System.out.format("%4s%16s%16s%16s","      4  ","   100","      4  ","    3\n");
+		 System.out.format("%4s%16s%16s%16s","      5  ","   101","      5  ","    4\n");
+		 System.out.format("%4s%16s%16s%16s","      6  ","   110","      6  ","    5\n");
+		 System.out.format("%4s%16s%16s%16s","      7  ","   111","      7  ","    6\n");
+		 System.out.format("%4s%16s%16s%16s","      8  ","   1000","      8  ","    7\n");
+		 System.out.format("%4s%16s%16s%16s","      9  ","   1001","      9  ","    8\n");
+		 System.out.format("%4s%16s%16s%16s","     10  ","   1010","    10  ","    9\n");
+		 System.out.format("%4s%16s%16s%16s","     11  ","   1011","     11  ","    A\n");	
+		 System.out.format("%4s%16s%16s%16s","     12  ","   1100","     12  ","    B\n");
+		 System.out.format("%4s%16s%16s%16s","     13  ","   1101","     13  ","    C\n");
+		 System.out.format("%4s%16s%16s%16s","     14  ","   1110","     14  ","    D\n");
+		 System.out.format("%4s%16s%16s%16s","     15  ","   1111","     15  ","    E\n");
+		 System.out.format("%4s%16s%16s%16s","     16  ","   10000","     16  ","    F\n");
+		 System.out.format("%4s%16s%16s%16s","     17  ","   10001","     17  ","    10\n");
+		 System.out.format("%4s%16s%16s%16s","     18  ","   10010","     18  ","    11\n");
+		 System.out.format("%4s%16s%16s%16s","     19  ","   10011","     19  ","    12\n");
+		 System.out.format("%4s%15s%16s%16s","     20   ","    10100","  20  ","   13\n");
+		
+	}
+
+}
