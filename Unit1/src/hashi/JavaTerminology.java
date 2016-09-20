056bd43348645a01e7ce1417db5d696c0ffd647b
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-09-20 12:53:47 -0400

    terminology

---
+++ Unit1/src/hashi/JavaTerminology.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package hashi;
+
+/**
+ * @author 324350081
+ *
+ */
+public class JavaTerminology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.format("%5s%5s", "    JavaTerminology", "\n");
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "1.","Execute:To run a program.\n");
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "2.", "Class: The description of an object.\n" );
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "3.", "Argument: Data passed to a method for processing.\n" );
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "4.","Comment: Text that provides information to reader of program code.\n" );
+		 System.out.format("\n");
+		 System.out.format("%5s%5s", "5.", "String: A set of characters.\n" );
+
+	}
+
+}
