caafe1e256a92d5e395c1c90c979bbc66a66c97f
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-10-25 10:56:31 -0400

    the cahnges have been made

--- Unit1/src/hashi/TicTacToeRules.java
+++ Unit1/src/hashi/TicTacToeRules.java
@@ -4,7 +4,8 @@
 package hashi;
 
 /**ICS3U
- * TicTacToe Rules 
+ * TicTacToe Rules.java
+ *  the rules for tictactoe 
  * Hashim Hashi
  *20/09/16
  */
@@ -15,19 +16,19 @@ public class TicTacToeRules {
 	 */
 	public static void main(String[] args) {
 		
-		 System.out.format("%5s%5s", "Tic Tac Toe Rules", "\n");
-		 System.out.format("\n");
-		 System.out.format(" Tic Tac Toe is a game for 2 players\n");
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "1.","A grid of 9 squares is drawn.\n");
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "2.", "The first player draws an X in an empty square.\n" );
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "3.", "The second player draws an O in an empty square.\n" );
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "4.","Steps 2 and 3 are repeated until a tic tac Pattern is created: Patterns can be diagnol, horizontal, or verical.\n" );
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "5.", "Aw innig pattern is completed by drawing a line through the pattern.\n" );
+		 System.out.println("Tic Tac Toe Rules\n");
+		
+		 System.out.println("Tic Tac Toe is a game for 2 players\n");
+	
+		 System.out.println( "1. A grid of 9 squares is drawn.\n");
+		 
+		 System.out.println( "2. The first player draws an X in an empty square.\n" );
+		 
+		 System.out.println( "3. The second player draws an O in an empty square.\n" );
+		 
+		 System.out.println( "4. Steps 2 and 3 are repeated until a tic tac Pattern is created: Patterns can be diagnol, horizontal, or verical.\n" );
+		 
+		 System.out.println( "5. Aw innig pattern is completed by drawing a line through the pattern.\n" );
 	}
 
 }
