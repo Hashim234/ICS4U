00678dafcc90e5d303b8bc3b1c7745dc5f417eae
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-10-26 13:19:39 -0400

    changes been made

--- Unit1/src/hashi/JavaTerminology.java
+++ Unit1/src/hashi/JavaTerminology.java
@@ -4,8 +4,9 @@
 package hashi;
 
 /**ICS3U
- * Java terminology 
- * hashim hashi
+ * Java terminology.java
+ *  Definitions for words form the java textbook
+ * Hashim hashi
  *20/09/16
  */
 public class JavaTerminology {
@@ -14,17 +15,17 @@ public class JavaTerminology {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 System.out.format("%5s%5s", "    JavaTerminology", "\n");
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "1.","Execute:To run a program.\n");
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "2.", "Class: The description of an object.\n" );
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "3.", "Argument: Data passed to a method for processing.\n" );
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "4.","Comment: Text that provides information to reader of program code.\n" );
-		 System.out.format("\n");
-		 System.out.format("%5s%5s", "5.", "String: A set of characters.\n" );
+		 System.out.println( "    JavaTerminology\n");
+	
+		 System.out.println( "1. Execute:To run a program.\n");
+	
+		 System.out.println( "2. Class: The description of an object.\n" );
+	
+		 System.out.println( "3. Argument: Data passed to a method for processing.\n" );
+		
+		 System.out.println( "4. Comment: Text that provides inprintlnion to reader of program code.\n" );
+		
+		 System.out.println( "5. String: A set of characters.\n" );
 
 	}
 
