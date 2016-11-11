613ba66357d691a2fad800eb51e812d507137721
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-11-11 13:55:13 -0500

    exercise 9 pg 96

---
+++ Unit2/src/hashi/GuessingGame.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package hashi;
+import java.util.Scanner;
+/**ICS3U
+ * Hashim Hashi
+ * GuessingGame.java
+ * guessing a number between 1 and 20
+ * 11/11/2016
+ */
+public class GuessingGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int number;
+		int correctguess =(int)(Math.random()*19)+2;
+		
+		System.out.println(" enter a number between 1 and 20");
+	number = sc. nextInt();
+	
+	if (correctguess==number){
+	System.out.println(" you won");
+	}
+	
+	if (number> correctguess){
+	System.out.println("Good try");
+	}
+	
+	else
+		System.out.println("Computers number: " + correctguess);
+	System.out.println("Players number: " + number);
+	
+	
+	
+	}
+	
+	
+
+}
