c4c8302bcd84ca71fd1bc9dc5fa0a20480eef482
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2016-11-24 15:56:26 -0500

    completed

---
+++ Unit 3/src/hashi/Chaos.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package hashi;
+import java.util.*;
+/**ICS3U
+ * Chaos.java
+ * this program
+ * hashim hashi
+ *24/11/16
+ */
+public class Chaos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		  Scanner sc = new Scanner(System.in);
+		 
+		  double total;
+		  
+		  System.out.print("Enter a number");
+		  total =sc.nextDouble(); 
+		  
+		  for(int i=0;i<50;i++){
+		  total= 2* total *(1 - total);
+		  System.out.println(total);
+			  
+		  }
+		  
+		  
+		  
+		  
+		  
+	}
+
+}
