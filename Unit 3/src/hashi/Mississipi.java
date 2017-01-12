5d92acf6dbce6a25870095d6f08687df4041d33b
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2017-01-12 13:13:27 -0500

    MISSISSIPPI EXERCISE

---
+++ Unit 3/src/hashi/Mississipi.java
@@ -0,0 +1,73 @@
+package hashi;
+/**
+ * 
+ */
+
+/**ICS3U
+ * Mississippi.java
+ * this program  spells out Mississippi vertically 
+ * Hashim Hashi
+ *11/01/2017
+ */
+public class Mississipi {
+public static void m(){
+	System.out.println("M       M");
+	System.out.println("M M   M M");
+	System.out.println("M M  M  M");
+	System.out.println("M   M   M");
+	System.out.println("M       M");
+	System.out.println("M       M");
+	System.out.println("M       M");
+	
+	
+	
+}
+public static void i(){
+	System.out.println(" IIIII");
+	System.out.println("   I");
+	System.out.println("   I");
+	System.out.println("   I");
+	System.out.println("   I");
+	System.out.println("   I");
+	System.out.println(" IIIII");
+	
+	
+}
+public static void s(){
+   System.out.println(" SSSSS");	
+   System.out.println("S     S");	
+   System.out.println("S");	
+   System.out.println(" SSSSS");	
+   System.out.println("      S");
+   System.out.println("S     S");	
+   System.out.println(" SSSSS");	
+}
+public static void p(){
+System.out.println("PPPPP");
+System.out.println("P    P");
+System.out.println("PPPPP");
+System.out.println("P ");
+System.out.println("P");
+System.out.println("P");
+}
+	/**
+	 * @param args
+
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        m();
+        i();
+        s();
+        s();
+        i();
+        s();
+        s();
+        i();
+        p();
+        p();
+        i();
+	}
+
+}
