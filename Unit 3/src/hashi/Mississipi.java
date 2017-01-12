db8fede9e4972068992d04bddfea38066beecc51
324350081 <324350081@C-DS24WR1.tdsb.on.ca>
2017-01-12 13:22:02 -0500

    comments added to each method

--- Unit 3/src/hashi/Mississipi.java
+++ Unit 3/src/hashi/Mississipi.java
@@ -10,64 +10,76 @@ package hashi;
  *11/01/2017
  */
 public class Mississipi {
-public static void m(){
-	System.out.println("M       M");
-	System.out.println("M M   M M");
-	System.out.println("M M  M  M");
-	System.out.println("M   M   M");
-	System.out.println("M       M");
-	System.out.println("M       M");
-	System.out.println("M       M");
-	
-	
-	
-}
-public static void i(){
-	System.out.println(" IIIII");
-	System.out.println("   I");
-	System.out.println("   I");
-	System.out.println("   I");
-	System.out.println("   I");
-	System.out.println("   I");
-	System.out.println(" IIIII");
-	
-	
-}
-public static void s(){
-   System.out.println(" SSSSS");	
-   System.out.println("S     S");	
-   System.out.println("S");	
-   System.out.println(" SSSSS");	
-   System.out.println("      S");
-   System.out.println("S     S");	
-   System.out.println(" SSSSS");	
-}
-public static void p(){
-System.out.println("PPPPP");
-System.out.println("P    P");
-System.out.println("PPPPP");
-System.out.println("P ");
-System.out.println("P");
-System.out.println("P");
-}
+	/**
+	 * draws the letter m
+	 */
+	public static void m(){
+		System.out.println("M       M");
+		System.out.println("M M   M M");
+		System.out.println("M M  M  M");
+		System.out.println("M   M   M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+
+
+
+	}
+	/**
+	 * draws the letter i
+	 */
+	public static void i(){
+		System.out.println(" IIIII");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println(" IIIII");
+
+
+	}
+	/**
+	 * draws the letter s 
+	 */
+	public static void s(){
+		System.out.println(" SSSSS");	
+		System.out.println("S     S");	
+		System.out.println("S");	
+		System.out.println(" SSSSS");	
+		System.out.println("      S");
+		System.out.println("S     S");	
+		System.out.println(" SSSSS");	
+	}
+	/**
+	 * draws the letter p
+	 */
+	public static void p(){
+		System.out.println("PPPPP");
+		System.out.println("P    P");
+		System.out.println("PPPPP");
+		System.out.println("P ");
+		System.out.println("P");
+		System.out.println("P");
+	}
 	/**
 	 * @param args
 
 	 */
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        m();
-        i();
-        s();
-        s();
-        i();
-        s();
-        s();
-        i();
-        p();
-        p();
-        i();
+		m();
+		i();
+		s();
+		s();
+		i();
+		s();
+		s();
+		i();
+		p();
+		p();
+		i();
 	}
 
 }
